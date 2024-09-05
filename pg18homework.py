71da7fca85ed688494f45f10b693afb62b47210a
LucyFinn <20.lucy.finn@msltcastlegregory.ie>
2024-09-05 20:34:32 +0100

    Update and rename pg 18 to pg18homework.py

---
+++ pg18homework.py
@@ -0,0 +1,13 @@
+#challenge 1
+age = 17
+print(age, "What a great age!")
+
+#challenge 5
+print()
+miles = 7100
+km = 1.60935
+print(miles*km)
+
+cost = 900
+answer = 11426.385
+print("€",cost*answer)
