a739245c5fccb9e3b76178c72c2fcffb721f9b9e
LucyFinn <20.lucy.finn@msltcastlegregory.ie>
2024-09-05 20:31:49 +0100

    Update and rename helloworld.py to basics.py

---
+++ basics.py
@@ -0,0 +1,15 @@
+print("Hello World!", end = ",")
+print("Hello World!")
+#the first line of code shows us how to write the same thing multiple times on the line
+
+print("Hello, \nit's me!")
+#this code allows any word put after \nit's on the next line
+
+print()
+#a print with empty brackets breaks up your code a bit
+print("\t* \t \t* \n \t \t* \n \t*********")
+
+print()
+name = "lucy"
+lastName = "o'mahony"
+print(name, lastName)
