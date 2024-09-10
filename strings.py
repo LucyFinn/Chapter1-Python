b78e7273295c51a12faf5f43ddb196aa82da820b
LucyFinn <20.lucy.finn@msltcastlegregory.ie>
2024-09-10 17:56:06 +0100

    Create strings.py

---
+++ strings.py
@@ -0,0 +1,29 @@
+name = input("Enter your name: ")
+print("HI",name)
+name = name.upper()
+print(name)
+name = name.lower()
+print(name)
+name = name.capitalize()
+print(name)
+lengthName = len(name)
+print(lengthName)
+
+print()
+string1 = "hello"
+string1 = string1.upper()
+print(string1)
+
+string2 = "GOODBYE"
+string2 = string2.lower()
+print(string2)
+
+string3 = "gORGEOUs"
+string3 = string3.capitalize()
+print(string3)
+
+string4 = "Hello"
+print(string4)
+length = len(string4)
+print(length)
+print(type(length))
