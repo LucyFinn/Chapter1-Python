16b54a2086eef1e1a7da07d10b431fdf5c15f3ec
LucyFinn <20.lucy.finn@msltcastlegregory.ie>
2024-09-26 11:01:58 +0100

    Create loopTasks.py

---
+++ loopTasks.py
@@ -0,0 +1,49 @@
+# #Task7
+# print("*******************")
+# print("*   My Products   *")
+# print("*******************")
+# print("*1. Curtains      *")
+# print("*2. Cushion Covers*")
+# print("*3. Quilts        *")
+# print("*4. Exit          *")
+# print("*******************")
+# 
+# option = int(input("Enter an option(1-4): "))
+# while (option != 4):
+#     if option == 1:
+#         colour = input("What colour curtains do you want? ")
+#         print("Yes I think", colour,"would be lovely on a curtain!!")
+#         print("Any other requirement please contact me at: lucy@sewing.com")
+#         option = 0
+#     elif option == 2:
+#         cushion = int(input("How many cushions would you like to order? "))
+#         print(cushion, "is a good amount, any other details contact my email: lucy@sewing.com")
+#         option = 0
+#     elif option == 3:
+#         quilt = input("Would you like a single or a double quilt? ")
+#         print("I can make a lovely", quilt, "for you!")
+#         print("Any other queries contact me at: lucy@sewing.com")
+#         option = 0
+#     else:
+#         print("Invalid option")
+#     print("*******************")
+#     print("*   My Products   *")
+#     print("*******************")
+#     print("*1. Curtains      *")
+#     print("*2. Cushion Covers*")
+#     print("*3. Quilts        *")
+#     print("*4. Exit          *")
+#     print("*******************")
+#     option = int(input("Enter an option(1-4): "))
+# print("Thank you for looking at my website!!")
+        
+
+#Task 6
+sentence = input("Please enter any sentence of your choice: ")
+spaceCount = sentence.count(" ")
+print(spaceCount)
+wordCount = 0
+while spaceCount > 0:
+    result = (spaceCount + 1)
+print(result)
+
