fe750593e94f85ac0cb217c863ecc1e12fc51faa
LucyFinn <20.lucy.finn@msltcastlegregory.ie>
2024-10-15 22:12:10 +0100

    Update randomGenerater.py

--- randomGenerater.py
+++ randomGenerater.py
@@ -7,7 +7,20 @@ nouns = ["teacher", "student", "principal", "library", "school"]
 verbs = ["taught", "learned", "read", "walked", "ran"]
 prepositions = ["to", "from", "over", "under", "on"]
 
-firstWord = random.choice(articles)
+article1 = random.choice(articles)
+noun1 = random.choice(nouns)
+verb1 = random.choice(verbs)
+article2 = random.choice(articles)
+noun2 = random.choice(nouns)
+prep = random.choice(prepositions)
+verb2 = random.choice(verbs)
 
-emptyList.append(firstWord)
+emptyList.append(article1)
+emptyList.append(noun1)
+emptyList.append(verb1)
+emptyList.append(article2)
+emptyList.append(noun2)
+emptyList.append(prep)
+emptyList.append(verb2)
+
 print(emptyList)
